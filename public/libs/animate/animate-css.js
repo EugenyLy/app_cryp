a713ea35e909bb4dc0db8cae604906b717495e9a
eugenypop <eugenypop@gmail.com>
2018-03-01 17:39:36 +0200

    Removed folder from repository

--- public/libs/animate/animate-css.js
+++
@@ -1,4 +0,0 @@
-//Animate CSS + WayPoints javaScript Plugin
-//Example: $(".element").animated("zoomInUp");
-//Author URL: http://webdesign-master.ru
-(function(a){a.fn.animated=function(b){a(this).each(function(){var c=a(this);c.css("opacity","0").addClass("animated").waypoint(function(d){"down"===d&&c.addClass(b).css("opacity","1")},{offset:"90%"})})}})(jQuery);
